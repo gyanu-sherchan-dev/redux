030df87377e2be21742a3eef43d8d10df4a4e133
Gyanu <gyanusherchan@outlook.com>
2023-06-13 22:53:11 +1000

    redux-toolkit, create and provide store,state slice and add to store

--- redux_demo/src/App.js
+++ redux_demo/src/App.js
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import "./App.css";
-import Counter from "./components/Counter";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const incr = () => {
+    setCount(count + 1);
+  };
+
+  const decr = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
   return (
     <div className="App">
-      <Counter />
+      <h1>Fresh code:</h1>
+
+      <h2>{count}</h2>
+      <div>
+        <button className="btn" onClick={incr}>
+          +
+        </button>
+
+        <button className="btn" onClick={decr}>
+          -
+        </button>
+      </div>
     </div>
   );
 }
---
+++ redux_demo/src/features/Counter.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./counter/CounterSlice";
+
+const Counter = () => {
+  return <div></div>;
+};
+
+export default Counter;
